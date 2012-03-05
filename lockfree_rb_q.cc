9c06be6d94e9ed703fdf0c2f97ab2733becfe9e1
Alexander K <ak@natsys-lab.com>
2012-03-05 19:53:34 +0400

    Move to C++11 from gcc intrinsics, cleanups

--- lockfree_rb_q.cc
+++ lockfree_rb_q.cc
@@ -2,8 +2,8 @@
  * Implementation of Naive and Lock-free ring-buffer queues and
  * performance and verification tests.
  *
- * Build with (g++ version must be >= 4.4.0):
- * $ g++ -Wall -std=c++0x -lpthread lockfree_rb_q.cc
+ * Build with (g++ version must be >= 4.5.0):
+ * $ g++ -Wall -std=c++0x -O2 -lpthread lockfree_rb_q.cc
  *
  * Copyright (C) 2012 Alexander Krizhanovsky (ak@natsys-lab.com).
  *
@@ -27,24 +27,18 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <atomic>
 #include <cassert>
 #include <iostream>
 #include <condition_variable>
 #include <mutex>
 #include <thread>
 
-#define QUEUE_SIZE	4096
+#define QUEUE_SIZE	(32 * 1024)
 
 /*
  * ------------------------------------------------------------------------
  * Naive serialized ring buffer queue
- *
- * std::list has O(n) size() complexity, so we use our own items counter.
- *
- * std::list is non-intrusive container, so additional allocations for
- * the list elemetns take place on new items insertions.
- * Also non-intrusive containers have bigger memory footprint and
- * worse cache locality due to additional hits on list linkage elements itself.
  * ------------------------------------------------------------------------
  */
 template<class T, unsigned long Q_SIZE = QUEUE_SIZE>
@@ -52,7 +46,11 @@ class NaiveQueue {
 public:
 	NaiveQueue()
 		: head_(0), tail_(0)
-	{}
+	{
+		ptr_array_ = (T **)::memalign(getpagesize(),
+				Q_SIZE * sizeof(void *));
+		assert(ptr_array_);
+	}
 
 	void
 	push(T *x)
@@ -63,7 +61,7 @@ public:
 					return tail_ + Q_SIZE > head_;
 				});
 
-		array_[head_++ % Q_SIZE] = x;
+		ptr_array_[head_++ % Q_SIZE] = x;
 
 		cond_empty_.notify_one();
 	}
@@ -77,7 +75,7 @@ public:
 					return tail_ < head_;
 				});
 
-		T *x = array_[tail_++ % Q_SIZE];
+		T *x = ptr_array_[tail_++ % Q_SIZE];
 
 		cond_overflow_.notify_one();
 
@@ -89,7 +87,7 @@ private:
 	std::condition_variable	cond_empty_;
 	std::condition_variable	cond_overflow_;
 	std::mutex		mtx_;
-	T *			array_[Q_SIZE];
+	T			**ptr_array_;
 };
 
 
@@ -145,34 +143,33 @@ public:
 	LockFreeQueue(size_t n_producers, size_t n_consumers)
 		: n_producers_(n_producers),
 		n_consumers_(n_consumers),
-		head(0),
-		tail(0),
-		last_head(0),
-		last_tail(0)
+		head_(0),
+		tail_(0),
+		last_head_(0),
+		last_tail_(0)
 	{
 		auto n = std::max(n_consumers_, n_producers_);
-		thr_p = (ThrPos *)::memalign(getpagesize(), sizeof(ThrPos) * n);
-		assert(thr_p);
+		thr_p_ = (ThrPos *)::memalign(getpagesize(), sizeof(ThrPos) * n);
+		assert(thr_p_);
 		// Set per thread tail and head to ULONG_MAX.
-		::memset((void *)thr_p, 0xFF, sizeof(ThrPos) * n);
+		::memset((void *)thr_p_, 0xFF, sizeof(ThrPos) * n);
 
-		ptr_array = (unsigned long *)::memalign(getpagesize(),
-				Q_SIZE * sizeof(long));
-		assert(ptr_array);
-		::memset(ptr_array, 0, Q_SIZE * sizeof(long));
+		ptr_array_ = (T **)::memalign(getpagesize(),
+				Q_SIZE * sizeof(void *));
+		assert(ptr_array_);
 	}
 
 	~LockFreeQueue()
 	{
-		::free(ptr_array);
-		::free(thr_p);
+		::free(ptr_array_);
+		::free(thr_p_);
 	}
 
 	ThrPos&
 	thr_pos() const
 	{
 		assert(ThrId() < std::max(n_consumers_, n_producers_));
-		return thr_p[ThrId()];
+		return thr_p_[ThrId()];
 	}
 
 	void
@@ -182,46 +179,38 @@ public:
 		 * Request next place to push.
 		 *
 		 * Second assignemnt is atomic only for head shift, so there is
-		 * a time window in which thr_p[tid].head = ULONG_MAX, and
+		 * a time window in which thr_p_[tid].head = ULONG_MAX, and
 		 * head could be shifted significantly by other threads,
-		 * so pop() will set last_head to head.
-		 * After that thr_p[tid].head is setted to old head value
+		 * so pop() will set last_head_ to head.
+		 * After that thr_p_[tid].head is setted to old head value
 		 * (which is stored in local CPU register) and written by @ptr.
 		 *
 		 * First assignment guaranties that pop() sees values for
-		 * head and thr_p[tid].head not greater that they will be
+		 * head and thr_p_[tid].head not greater that they will be
 		 * after the second assignment with head shift.
 		 */
-		thr_pos().head = head;
-		thr_pos().head = __sync_fetch_and_add(&head, 1);
+		thr_pos().head = head_.load(std::memory_order_relaxed);
+		thr_pos().head = head_.fetch_add(1);
 
 		/*
 		 * We do not know when a consumer uses the pop()'ed pointer,
 		 * se we can not overwrite it and have to wait the lowest tail.
 		 */
-		while (__builtin_expect(thr_pos().head >= last_tail + Q_SIZE, 0)) {
-			auto min = ULONG_MAX, old_dt = last_tail;
-			// Synchronize to ensure that we find real minimum.
-			__sync_synchronize();
-			// Update the last_tail.
+		while (__builtin_expect(thr_pos().head >= last_tail_ + Q_SIZE, 0))
+		{
+			::sched_yield();
+
+			auto min = tail_.load(std::memory_order_relaxed);
+			// Update the last_tail_.
 			for (size_t i = 0; i < n_consumers_; ++i) {
-				auto tmp_t = thr_p[i].tail;
+				auto tmp_t = thr_p_[i].tail;
 				if (tmp_t < min)
 					min = tmp_t;
 			}
-			// Do not change last_tail if it's changed by s.b. else.
-			if (min != ULONG_MAX)
-				__sync_val_compare_and_swap(&last_tail, old_dt, min);
-			// Check again with updated last_tail.
-			if (thr_pos().head >= last_tail + Q_SIZE)
-				//::usleep(1000); // sleep for one OS schedulter tick
-				::sched_yield();
-			else
-				break;
+			last_tail_ = min;
 		}
 
-		ptr_array[thr_pos().head & Q_MASK]
-			= reinterpret_cast<unsigned long>(ptr);
+		ptr_array_[thr_pos().head & Q_MASK] = ptr;
 		// Allow consumers eat the item.
 		thr_pos().head = ULONG_MAX;
 	}
@@ -229,50 +218,52 @@ public:
 	T *
 	pop()
 	{
-		// Request next place from which to pop.
-		thr_pos().tail = __sync_fetch_and_add(&tail, 1);
+		/*
+		 * Request next place from which to pop.
+		 * See comments for push().
+		 */
+		thr_pos().tail = tail_.load(std::memory_order_relaxed);
+		thr_pos().tail = tail_.fetch_add(1);
 
 		/*
-		 * tid'th place in ptr_array is reserved by the thread -
+		 * tid'th place in ptr_array_ is reserved by the thread -
 		 * this place shall never be rewritten by push() and
-		 * last_tail at push() is a guarantee.
-		 * last_head guaraties that no any consumer eats the item
+		 * last_tail_ at push() is a guarantee.
+		 * last_head_ guaraties that no any consumer eats the item
 		 * before producer reserved the position writes to it.
 		 */
-		while (__builtin_expect(thr_pos().tail >= last_head, 0)) {
-			auto min = head, old_dh = last_head;
-			// Synchronize to ensure that we find real minimum.
-			__sync_synchronize();
-			// Update the last_head.
+		while (__builtin_expect(thr_pos().tail >= last_head_, 0))
+		{
+			::sched_yield();
+
+			auto min = head_.load(std::memory_order_relaxed);
+			// Update the last_head_.
 			for (size_t i = 0; i < n_producers_; ++i) {
-				auto tmp_h = thr_p[i].head;
+				auto tmp_h = thr_p_[i].head;
 				if (tmp_h < min)
 					min = tmp_h;
 			}
-			// Do not change last_head if it's changed by s.b. else.
-			__sync_val_compare_and_swap(&last_head, old_dh, min);
-			// Check again with updated last_head.
-			if (thr_pos().tail < last_head)
-				break;
-			//::usleep(1000); // sleep for one OS schedulter tick
-			::sched_yield();
+			last_head_ = min;
 		}
 
-		return reinterpret_cast<T *>(ptr_array[thr_pos().tail & Q_MASK]);
+		T *ret = ptr_array_[thr_pos().tail & Q_MASK];
+		// Allow producers rewrite the slot.
+		thr_pos().tail = ULONG_MAX;
+		return ret;
 	}
 
 private:
 	const size_t n_producers_, n_consumers_;
 	// currently free position (next to insert)
-	volatile unsigned long	head;
+	std::atomic<unsigned long> head_;
 	// current tail, next to pop
-	volatile unsigned long	tail;
+	std::atomic<unsigned long> tail_;
 	// last not-processed producer's pointer
-	volatile unsigned long	last_head;
+	volatile unsigned long	last_head_;
 	// last not-processed consumer's pointer
-	volatile unsigned long	last_tail;
-	ThrPos	*thr_p;
-	unsigned long * volatile ptr_array;
+	volatile unsigned long	last_tail_;
+	ThrPos		*thr_p_;
+	T		**ptr_array_;
 };
 
 
@@ -281,22 +272,20 @@ private:
  *	Tests for naive and lock-free queues
  * ------------------------------------------------------------------------
  */
-static const auto N = QUEUE_SIZE * 512;
-static const auto CONSUMERS = 2;
-static const auto PRODUCERS = 2;
+static const auto N = QUEUE_SIZE * 1024;
+static const auto CONSUMERS = 16;
+static const auto PRODUCERS = 16;
 
 typedef unsigned char	q_type;
 
 static const q_type X_EMPTY = 0; // the address skipped by producers
 static const q_type X_MISSED = 255; // the address skipped by consumers
-static q_type x[N * PRODUCERS];
-static int n = 0;
-
-std::mutex io_mtx;
+q_type x[N * PRODUCERS];
+std::atomic<int> n(0);
 
 template<class Q>
-struct Callable {
-	Callable(Q *q, size_t id = 0)
+struct Worker {
+	Worker(Q *q, size_t id = 0)
 		: q_(q),
 		thr_id_(id)
 	{}
@@ -306,98 +295,73 @@ struct Callable {
 };
 
 template<class Q>
-struct Producer : public Callable<Q> {
+struct Producer : public Worker<Q> {
 	Producer(Q *q, size_t id)
-		: Callable<Q>(q, id)
+		: Worker<Q>(q, id)
 	{}
 
 	void operator()()
 	{
-		set_thr_id(Callable<Q>::thr_id_);
+		set_thr_id(Worker<Q>::thr_id_);
 
-		{
-			std::lock_guard<std::mutex> lock(io_mtx);
-			std::cout << "run producer " << thr_id() << std::endl;
-		}
-
-		size_t i = 0;
-		for (i = thr_id(); i < N * PRODUCERS; i += PRODUCERS) {
+		for (auto i = thr_id(); i < N * PRODUCERS; i += PRODUCERS) {
 			x[i] = X_MISSED;
-			Callable<Q>::q_->push(x + i);
+			Worker<Q>::q_->push(x + i);
 		}
-		std::lock_guard<std::mutex> lock(io_mtx);
-		std::cout << "producer " << thr_id() << "(" << pthread_self()
-			 << ") finished (pushed " << (i / PRODUCERS)
-			 << ")" << std::endl;
 	}
 };
 
 template<class Q>
-struct Consumer : public Callable<Q> {
+struct Consumer : public Worker<Q> {
 	Consumer(Q *q, size_t id)
-		: Callable<Q>(q, id)
+		: Worker<Q>(q, id)
 	{}
 
 	void operator()()
 	{
-		set_thr_id(Callable<Q>::thr_id_);
+		set_thr_id(Worker<Q>::thr_id_);
 
-		{
-			std::lock_guard<std::mutex> lock(io_mtx);
-			std::cout << "run consumer " << thr_id() << std::endl;
-		}
-
-		while ((unsigned)__sync_add_and_fetch(&n, 1) <= N * PRODUCERS) {
-			q_type *v = Callable<Q>::q_->pop();
+		while (n.fetch_add(1) < N * PRODUCERS) {
+			q_type *v = Worker<Q>::q_->pop();
 			assert(v);
 			assert(*v == X_MISSED);
 			*v = (q_type)(thr_id() + 1); // don't write zero
 		}
-		std::lock_guard<std::mutex> lock(io_mtx);
-		std::cout << "consumer " << thr_id() << "(" << pthread_self()
-			<< ") finished" << std::endl;
 	}
 };
 
-// TODO
-//	-- add time measurements
-//	+- dummy queue
-//	-- GCC atomic intrinsics -> C++11
 template<class Q>
 void
-run_test(Q &q)
+run_test(Q &&q)
 {
 	std::thread thr[PRODUCERS + CONSUMERS];
 
+	n.store(0);
 	::memset(x, X_EMPTY, N * sizeof(q_type) * PRODUCERS);
 
-	std::cout << "run producers..." << std::endl;
+	// Run producers.
 	for (auto i = 0; i < PRODUCERS; ++i)
 		thr[i] = std::thread(Producer<Q>(&q, i));
 
-	std::cout << "sleep little bit..." << std::endl;
 	::usleep(10 * 1000); // sleep to wait the queue is full
 
-	std::cout << "run consumers..." << std::endl;
+	/*
+	 * Run consumers.
+	 * Create consumers with the same thread IDs as producers.
+	 * The IDs are used for queue head and tail indexing only,
+	 * so we  care only about different IDs for threads of the same type.
+	 */
 	for (auto i = 0; i < CONSUMERS; ++i)
-		/*
-		 * Create consumers with the same thread IDs as producers.
-		 * The IDs are used for queue head and tail indexing only,
-		 * so we  care only about different IDs for threads of
-		 * the same type.
-		 */
 		thr[PRODUCERS + i] = std::thread(Consumer<Q>(&q, i));
 
-	{
-		std::lock_guard<std::mutex> lock(io_mtx);
-		std::cout << "wait for completion..." << std::endl;
-	}
+	// Wait for all threads completion.
 	for (auto i = 0; i < PRODUCERS + CONSUMERS; ++i)
 		thr[i].join();
 
+	// Check data.
 	auto res = 0;
 	std::cout << "check X data..." << std::endl;
-	for (auto i = 0; i < N * PRODUCERS; ++i)
+	for (auto i = 0; i < N * PRODUCERS; ++i) {
 		if (x[i] == X_EMPTY) {
 			std::cout << "empty " << i << std::endl;
 			res = 1;
@@ -407,6 +371,7 @@ run_test(Q &q)
 			res = 2;
 			break;
 		}
+	}
 	std::cout << (res ? "FAILED" : "Passed") << std::endl;
 }
 
@@ -414,10 +379,10 @@ int
 main()
 {
 	LockFreeQueue<q_type> lf_q(PRODUCERS, CONSUMERS);
-	run_test<LockFreeQueue<q_type>>(lf_q);
+	run_test<LockFreeQueue<q_type>>(std::move(lf_q));
 
-	//NaiveQueue<q_type> n_q;
-	//run_test<NaiveQueue<q_type>>(n_q);
+	NaiveQueue<q_type> n_q;
+	run_test<NaiveQueue<q_type>>(std::move(n_q));
 
 	return 0;
 }
